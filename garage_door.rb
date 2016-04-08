4db038128d9e84665a19c2e57b1036cc9852a37e
bwilkins <bwilkins@covermymeds.com>
2016-04-08 15:25:13 -0400

    initial completion of dailyprogrammer challenge

---
+++ garage_door.rb
@@ -0,0 +1,36 @@
+class Door
+  attr_reader :file, :state
+  STATES = {
+    open: 'open',
+    closed: 'closed',
+    opening: 'opening',
+    closing: 'closing',
+    ostopped: 'stopped_while_opening',
+    cstopped: 'stopped_while_closing'
+  }
+
+  def initialize(file='door.txt')
+    @file = file
+    @state = STATES[:open]
+  end
+
+  def push_button
+    case state
+    when STATES[:open] then closing
+    when STATES[:closed] then opening
+    when STATES[:opening] then ostopped
+    when STATES[:closing] then cstopped
+    when STATES[:ostopped] then closing
+    when STATES[:cstopped] then opening
+    end
+  end
+
+  def finish
+    return if state != STATES[:closing] && state != STATES[:opening]
+    state == STATES[:closing] ? closed : open
+  end
+
+  STATES.each_key do |k|
+    define_method(k) { @state = STATES[k] }
+  end
+end
---
+++ garage_door_spec.rb
@@ -0,0 +1,79 @@
+require 'pry'
+require 'rspec'
+require_relative './garage_door'
+
+describe Door do
+  it 'writes to a file'
+
+  let(:door) { described_class.new }
+  describe '#push_button' do
+    it 'changes to closing when open' do
+      door.open
+      door.push_button
+      expect(door.state).to eq 'closing'
+    end
+
+    it 'when closing, it changes to stopped_while_closing' do
+      door.closing
+      door.push_button
+      expect(door.state).to eq 'stopped_while_closing'
+    end
+
+    it 'when pushed while stopped, it opens' do
+      door.cstopped
+      door.push_button
+      expect(door.state).to eq 'opening'
+    end
+
+    it 'changes to opening when closed' do
+      door.closed
+      door.push_button
+      expect(door.state).to eq 'opening'
+    end
+
+    it 'when opening, it changes to stopped_while_opening' do
+      door.opening
+      door.push_button
+      expect(door.state).to eq 'stopped_while_opening'
+    end
+
+    it 'when pushed while stopped_while_opening, it closes' do
+      door.ostopped
+      door.push_button
+      expect(door.state).to eq 'closing'
+    end
+  end
+
+  describe '#finish' do
+    it 'eventually it should be closed' do
+      door.push_button
+      door.finish
+      expect(door.state).to eq 'closed'
+    end
+
+    it 'opens when opening' do
+      door.opening
+      door.finish
+      expect(door.state).to eq 'open'
+    end
+
+    it 'returns nil if not closing or opening' do
+      door.cstopped
+      door.finish
+      expect(door.state).to eq 'stopped_while_closing'
+    end
+  end
+
+  context 'when new' do
+    it 'state will be open' do
+      expect(door.state).to eq 'open'
+    end
+  end
+
+  context 'when closed' do
+    before { door.closed }
+    it 'state will be closed' do
+      expect(door.state).to eq 'closed'
+    end
+  end
+end
